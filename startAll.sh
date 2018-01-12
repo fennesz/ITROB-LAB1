bff269af1c5b7cfa12c4edd96c339f73164498e9
Martin <mcfennesz@gmail.com>
2018-01-12 04:19:30 -0800

    Created script to start all

---
+++ startAll.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+cd ~/catkin_ws
+roscore &
+sleep 1
+catkin_make
+sleep 1
+xterm -e "bash -c \"roslaunch tenderbot TenderBot.launch; exec bash\"" &
+sleep 1
+python ~/catkin_ws/src/ITROB-LAB1/tenderbot_vision/opencvlibrary/visiongui.py ros
+cd -
+./killAll.sh
